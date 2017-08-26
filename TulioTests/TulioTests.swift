6b977a2095e994c5e1c0206cce2998962ffae5f6
Julio <juliomb89@gmail.com>
2017-08-26 11:30:35 +0200

    Added example test

--- TulioTests/TulioTests.swift
+++ TulioTests/TulioTests.swift
@@ -13,24 +13,19 @@ class TulioTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testParseCars() {
+        let carResultsInteractor = CarResultsInteractor(apiConsumer: HotwireAPIConsumerFakeImpl())
+        carResultsInteractor.execute(withSearchParams: SearchParams(place: "fake", fromDate: Date(), untilDate: Date()), completion: { carResults in
+            XCTAssert(carResults.count > 0)
+        }, onError: { error in
+            XCTFail("Failed with error: \(error)")
+        })
     }
     
 }
